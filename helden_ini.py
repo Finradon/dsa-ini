bf2aad401be33d1d12d7ca3b5610e0b8824ffdc1
Benedict Harder <benedictharder@posteo.de>
2024-12-28 14:01:59 +0100

    remove json import

--- helden_ini.py
+++ helden_ini.py
@@ -19,9 +19,11 @@ if 'ini_idx' not in st.session_state:
 if 'round' not in st.session_state:
     st.session_state['round'] = 1 # round counter
 
+bt_funcs.sort_enemies()
+
 # top buttons, general interaction
 with st.container(border=True):
-    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns([3, 3, 3, 3, 1, 1, 1, 2])
+    col1, col2, col3, col4, col5, col6, col7 = st.columns([3, 3, 3, 3, 1, 1, 2])
 
     with col1:
         ccol1, ccol2 = st.columns(2) # no idea why this is, but if you rename these to "col1", it doesn't work anymore
@@ -50,17 +52,14 @@ with st.container(border=True):
             hero_name = st.selectbox("Helden hinzufügen", bt_funcs.get_names_from_dir("json-samples/helden"))
         with col2:
             st.button('🦸', on_click=bt_funcs.add_hero_from_name, kwargs={"name": hero_name})
-    
-    with col5:
-        button3 = st.button('Sort', on_click=bt_funcs.sort_enemies)
 
-    with col6:
+    with col5:
         button4 = st.button('⏭️', on_click=bt_funcs.next)
     
-    with col7:
+    with col6:
         button5 = st.button('🔄', on_click=bt_funcs.reset)
 
-    with col8:  
+    with col7:  
         st.header(f"Runde: {st.session_state['round']}")
 
 
@@ -140,12 +139,12 @@ with ini_container:
                 remove_button = st.button("❌", key = i + 600, on_click=bt_funcs.remove_entity, kwargs={"ent": element})
                 
 # JSON import
-with st.container(border=True):
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        files = st.file_uploader("Importiere eigene Charaktere:", type='json', accept_multiple_files=True)
-    with col2:
-        option = st.selectbox("import options", ("Gegner (Nahkampf)", "Gegner (Fernkampf)", "Dämon"), label_visibility="collapsed")
-    with col3:
-        import_button = st.button('Import', on_click=bt_funcs.process_files, kwargs={"file_items": files, "class_name": option})
+# with st.container(border=True):
+#     col1, col2, col3 = st.columns(3)
+#     with col1:
+#         files = st.file_uploader("Importiere eigene Charaktere:", type='json', accept_multiple_files=True)
+#     with col2:
+#         option = st.selectbox("import options", ("Gegner (Nahkampf)", "Gegner (Fernkampf)", "Dämon"), label_visibility="collapsed")
+#     with col3:
+#         import_button = st.button('Import', on_click=bt_funcs.process_files, kwargs={"file_items": files, "class_name": option})
 
